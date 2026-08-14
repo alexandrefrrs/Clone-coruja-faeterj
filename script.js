77d05061809a8cb63321989b95183add2c4526a0
xandefs4 <xandeferreiradasilva@gmail.com>
2026-08-13 22:29:54 -0300

    refatorando

--- script.js
+++ script.js
@@ -1,36 +1,38 @@
+const campo = document.querySelectorAll('.campo-amarelo'); // Efeito amarelado quando campo esta preenchido 
 const campos = document.querySelectorAll('.campo-amarelo'); // Efeito amarelado quando campo esta preenchido 
 const formulario = document.getElementById('dados-login');
-
-let login = document.getElementById("login");
-let senha = document.getElementById("Senha");
-
-    if(login.value.trim()==""){
-    console.log("O nome é obrigatório"); 
-            
-    return false;
-    }
-
-    if(senha.value.trim()==""){
-    console.log("A senha é obrigatório"); 
-            
-    return false;
-    }   
-                
-    console.log("Login válido!");
-    return true;
-                            
-
-    campos.forEach(campo => {
+const login = document.getElementById('login');
+const senha = document.getElementById('senha');
+    
+        campos.forEach(campo => {
                        
-    campo.addEventListener('blur', function() {
+        campo.addEventListener('blur', function() {
 
-    if (this.value.trim() !== "") {
-    this.classList.add('preenchido'); // Se tiver texto, adiciona a classe
-    } 
-        else {
-        this.classList.remove('preenchido'); // Se estiver vazio, remove
-        }
+        if (this.value.trim() !== "") {
+        this.classList.add('preenchido'); // Se tiver texto, adiciona a classe
+        } 
+            else {
+            this.classList.remove('preenchido'); // Se estiver vazio, remove
+            }
         
+        });
+        });
+    
+    formulario.addEventListener('submit', function(eventoCapturado) {
+    eventoCapturado.preventDefault(); 
+      
+        if(login.value.trim()==""){
+        console.log("O nome é obrigatório"); 
+            
+        return;
+        }
+
+        if(senha.value.trim()==""){
+        console.log("A senha é obrigatório"); 
+            
+        return;
+        }   
+                
+        console.log("Login válido!");
+        return;
     });
-    });
-       
